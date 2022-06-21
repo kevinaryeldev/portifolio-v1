b1b16037d8f52163598fbcdd25c44235701e2aec
Kevin <kevinaryel.dev@gmail.com>
2022-06-21 09:07:58 -0300

    feat:About section created

---
+++ src/components/About/index.jsx
@@ -0,0 +1,35 @@
+import { AboutContainer, AboutGrid, AboutSection } from './style'
+
+const About = () => {
+  return (
+    <AboutContainer id="about">
+      <AboutGrid>
+        <AboutSection columnNumber={'1/span 2'}>
+          <h2>About me</h2>
+          <p>
+            I currently study system analysis and development at an brazilian
+            university and recently completed an 1000h course on web
+            development. I keep only studying react and some libraries while i
+            don't find an job on web dev but i hope begin to work with
+            programming soon. If you want to see more, here is my{' '}
+            <a href="#">CV</a>
+          </p>
+        </AboutSection>
+        <AboutSection columnNumber={'1'}>
+          <h2> About this project</h2>
+          <p>
+            This page is now in a "experimental state", i'm using this
+            enviroment to explore some elements that i haven't had the pleasure
+            of using before. I recommend that you stay tuned here because soon
+            we will have good changes
+          </p>
+        </AboutSection>
+
+        <AboutSection columnNumber={'2'}>
+          <h2>Comming soon</h2>
+        </AboutSection>
+      </AboutGrid>
+    </AboutContainer>
+  )
+}
+export default About
