805ca3d48ea2dd8ea8e95157bc67227cae7e81d7
Kevin <kevinaryel.dev@gmail.com>
2022-06-21 09:29:41 -0300

    feat(Projects):Projects now linked to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Home />
         <About />
+        <Projects />
       </div>
     </>
   )
