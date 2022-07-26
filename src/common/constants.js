9bd94109016e5321112735ced7df4924f6756af1
Kevin <kevinaryel.dev@gmail.com>
2022-07-26 20:41:21 -0300

    feat(Projects): Projects constant to create cards

---
+++ src/common/constants.js
@@ -0,0 +1,23 @@
+export const ProjectsData = [
+  {
+    title: 'KenzieFood',
+    link: 'https://kevinaryeldev.github.io/api-kenziefood-m2-lancao2/',
+    githubLink: 'https://github.com/kevinaryeldev/api-kenziefood-m2-lancao2',
+    description: 'Projeto de Crud em uma api simulando um e-commerce.',
+    img: 'https://i.ibb.co/h7KVqWn/Kenzie-Food.png',
+  },
+  {
+    title: 'Cardápio Digital',
+    link: 'https://cardapio-digital-capstone-kevinaryeldev.vercel.app',
+    githubLink: 'https://github.com/kevinaryeldev/cardapio-digital-capstone',
+    description: 'Projeto de cardápio digital com  gerenciamento de pedidos',
+    img: 'https://i.ibb.co/VTTdS4V/Cardapio-Digital.png',
+  },
+  {
+    title: 'Whats Next',
+    link: 'https://whats-next-kevin.vercel.app',
+    githubLink: 'https://github.com/kevinaryeldev/whats-next',
+    description: 'Projeto pessoal de uma to do list com redux e chakra-ui',
+    img: 'https://i.ibb.co/PZKzJFs/Whats-Next.png',
+  },
+]
